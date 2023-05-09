37b6eb7ba7ca102947edca2c41f42381f655678e
gapopke2860 <133056463+gapopke2860@users.noreply.github.com>
2023-05-09 17:16:11 -0400

    Update streamlit_app.py
    Added special menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal\n🥗Kale, Spinach & Rocket Smoothie\n🐔Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
