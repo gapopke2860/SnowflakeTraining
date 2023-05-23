98d40912ab966c8685f98774991130477dc9d739
gapopke2860 <133056463+gapopke2860@users.noreply.github.com>
2023-05-23 12:21:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector as sf
 
-# Connect to Snowflake
 con = sf.connect(
     user=st.secrets["snowflake"]["user"],
     password=st.secrets["snowflake"]["password"],
@@ -12,28 +11,13 @@ con = sf.connect(
     schema=st.secrets["snowflake"]["schema"]
 )
 
-# Fetch all the data from Snowflake
+st.title('My Parents New Healthy Diner')
+
+st.header('Car Filter Menu')
+
+# Fetch all the data, instead of limiting to 50
 query = "SELECT * FROM DEMO_DB.PUBLIC.CARS_DATASET"
 df = pd.read_sql_query(query, con)
-
-# Set the number of cars to display per page
-items_per_page = 50
-
-# Get the total number of cars
-total_cars = len(df)
-
-# Calculate the total number of pages
-total_pages = total_cars // items_per_page + 1
-
-# Get the current page from the user
-current_page = st.number_input("Page", min_value=1, max_value=total_pages, value=1)
-
-# Calculate the start and end indices for the current page
-start_index = (current_page - 1) * items_per_page
-end_index = start_index + items_per_page
-
-# Slice the DataFrame to get the cars for the current page
-df_page = df.iloc[start_index:end_index]
 
 # Let user define filtering conditions
 price_range = st.slider("Price range", float(df["PRICE"].min()), float(df["PRICE"].max()), (float(df["PRICE"].min()), float(df["PRICE"].max())))
@@ -47,24 +31,14 @@ df_filtered = df[(df["PRICE"].between(*price_range)) &
                  (df["TRANSMISSION"] == transmission) &
                  (df["FUEL_TYPE"] == fuel_type)]
 
-# Get the number of cars to display per page from the user
+# Get the number of rows to display from user
 rows = st.number_input("Number of rows to display", min_value=10, max_value=50, value=10, step=10)
 
 # Display the number of cars in the current selection
 st.text(f"Number of cars in the selection: {len(df_filtered)}")
 
-# Display the cars for the current page
-with st.beta_expander("Cars"):
-    for index, row in df_page.iterrows():
-        st.write(f"Car {index}: {row['MAKE']} {row['MODEL']} - Price: ${row['PRICE']}")
-
-# Display pagination controls
-if total_pages > 1:
-    st.write("Page:", current_page)
-    if current_page > 1:
-        st.button("Previous")
-    if current_page < total_pages:
-        st.button("Next")
+# Display the filtered DataFrame, limit the number of rows based on user's input
+st.dataframe(df_filtered.head(rows))
 
 st.text('🥑🍞 Avocado Toast')
 
@@ -78,7 +52,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
 
-# Fruityvice API section
+#fruityvice api section
 st.header('Fruityvice Fruit Advice!')
 fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
 st.write('The user entered', fruit_choice)
@@ -86,7 +60,7 @@ st.write('The user entered', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# Normalize JSON response
+#normalize json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 st.dataframe(fruityvice_normalized)
